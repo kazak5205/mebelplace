c3bf190d88762ffa0ce4bc03c59c281c7fa2524b
MebelPlace <admin@mebelplace.com.kz>
2025-10-29 23:13:58 +0500

    chore: remove React Native version - Flutter is the main mobile app

--- mobile/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-// Mobile UI Components
-// RN equivalents of shared components
-
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-
-// Re-export types
-export type { default as ButtonProps } from './Button';
-export type { default as InputProps } from './Input';
